bb79ef3131453b0ecb82b1a8f40d6f3df17275b3
Joseph <GMRemie@users.noreply.github.com>
2021-06-07 15:17:19 -0400

    XCode Init

---
+++ Rocket News/Rocket News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rocket News
+//
+//  Created by Joseph Storer on 6/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
