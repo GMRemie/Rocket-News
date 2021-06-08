57d51317ff1531ef4fc384f304b7604ff27a53d2
Joseph <GMRemie@users.noreply.github.com>
2021-06-08 14:19:41 -0400

    Nib creation
    Gonna be using nibs for tableViewCells for reusability.

---
+++ Rocket News/Rocket News/Views/ArticleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleTableViewCell.swift
+//  Rocket News
+//
+//  Created by Joseph Storer on 6/8/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
