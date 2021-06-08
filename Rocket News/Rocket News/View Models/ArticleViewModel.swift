2ecedd863e3c5870845b46e817b8b6405877b700
Joseph <GMRemie@users.noreply.github.com>
2021-06-07 21:30:40 -0400

    Light commentation

--- Rocket News/Rocket News/View Models/ArticleViewModel.swift
+++ Rocket News/Rocket News/View Models/ArticleViewModel.swift
@@ -12,6 +12,10 @@ import UIKit
 class ArticleViewModel {
 
     var progress:CGFloat = 0.0
+    
+    // The estimated completion float is based on the size of the web page without extraneous sections IE comment boxes.
+    // Typically from this API around 65% of the page is accurate. If given more time, you could probably take the actual article height - comment section for a more
+    // accurate representation.
     var estimateCompleted:CGFloat = 65.0
 
     
